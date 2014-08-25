89c6deb9ac757e7b1802e86c7dbf97c3e5ee2010
axzxc1236 <axzxc1236@gmail.com>
2014-08-25 17:53:23 +0800

    r1
    Release1

---
+++ Form1.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "匯入檔案"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(108, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "匯出紀錄"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(12, 129)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(418, 88)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(204, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "增加指令"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(300, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "刪除指令"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(396, 13)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "編輯指令"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 114)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 12)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "指令列表:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 245)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 12)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "輸出結果:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 261)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(458, 88)
+        Me.TextBox1.TabIndex = 10
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(437, 129)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(33, 23)
+        Me.Button6.TabIndex = 11
+        Me.Button6.Text = "↑"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(438, 194)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(33, 23)
+        Me.Button7.TabIndex = 12
+        Me.Button7.Text = "↓"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 12)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "X"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(106, 42)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 12)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Y"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(202, 42)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(12, 12)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Z"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 57)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(75, 22)
+        Me.TextBox2.TabIndex = 16
+        Me.TextBox2.Text = "~"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(108, 57)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(75, 22)
+        Me.TextBox3.TabIndex = 17
+        Me.TextBox3.Text = "~"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(204, 57)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(75, 22)
+        Me.TextBox4.TabIndex = 18
+        Me.TextBox4.Text = "~"
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(300, 55)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 19
+        Me.Button8.Text = "產生代碼"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(396, 55)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 23)
+        Me.Button9.TabIndex = 20
+        Me.Button9.Text = "複製代碼"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 362)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "CommandCart Helper"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,155 @@
+﻿Public Class Form1
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Add(InputBox("請輸入欲新增的指令"))
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim ifdelete As UShort
+        ifdelete = MsgBox("是否要刪除該指令:" & vbCrLf & ListBox1.Text, MsgBoxStyle.YesNo)
+        If ifdelete = 6 Then ListBox1.Items.Remove(ListBox1.Text)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged, Me.Load
+        If ListBox1.SelectedIndex = -1 Then
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+        Else
+            Button4.Enabled = True
+            Button5.Enabled = True
+            If Not ListBox1.Items.Count = 1 And Not ListBox1.SelectedIndex = 0 Then
+                Button6.Enabled = True
+            Else
+                Button6.Enabled = False
+            End If
+            If Not ListBox1.Items.Count = 1 And Not ListBox1.SelectedIndex + 1 = ListBox1.Items.Count Then
+                Button7.Enabled = True
+            Else
+                Button7.Enabled = False
+            End If
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click, ListBox1.DoubleClick
+        If ListBox1.SelectedIndex = -1 Then
+            Exit Sub
+        End If
+        Dim newcommand As String
+        Dim index As Integer
+        newcommand = InputBox("請輸入新指令")
+        If newcommand <> "" Then
+            index = ListBox1.SelectedIndex
+            ListBox1.Items.RemoveAt(index)
+            ListBox1.Items.Insert(index, newcommand)
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim text1, text2 As String
+        Dim index As Integer
+        index = ListBox1.SelectedIndex
+        text1 = ListBox1.Items.Item(index - 1)
+        text2 = ListBox1.Items.Item(index)
+
+        ListBox1.Items.RemoveAt(index - 1)
+        ListBox1.Items.Insert(index - 1, text2)
+
+        ListBox1.Items.RemoveAt(index)
+        ListBox1.Items.Insert(index, text1)
+
+        ListBox1.SelectedIndex = index - 1
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim text1, text2 As String
+        Dim index As Integer
+        index = ListBox1.SelectedIndex
+        text1 = ListBox1.Items.Item(index)
+        text2 = ListBox1.Items.Item(index + 1)
+
+        ListBox1.Items.RemoveAt(index + 1)
+        ListBox1.Items.Insert(index + 1, text1)
+
+        ListBox1.Items.RemoveAt(index)
+        ListBox1.Items.Insert(index, text2)
+
+        ListBox1.SelectedIndex = index + 1
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If ListBox1.Items.Count = 0 Then
+            MsgBox("嗯...請先加幾個指令再用此功能吧!")
+        End If
+        If ListBox1.Items.Count = 1 Then
+            TextBox1.Text = "summon MinecartCommandBlock " & TextBox2.Text & " " & TextBox3.Text & " " & TextBox4.Text & " {Command:" & """" & ListBox1.Items.Item(0) & """" & "}"
+        End If
+        If ListBox1.Items.Count > 1 Then
+            TextBox1.Text = "summon MinecartCommandBlock " & TextBox2.Text & " " & TextBox3.Text & " " & TextBox4.Text & " "
+            For i = 0 To ListBox1.Items.Count - 2
+                TextBox1.Text = TextBox1.Text & "{Riding:"
+            Next
+
+            '第一個指令
+            TextBox1.Text = TextBox1.Text & "{id:MinecartCommandBlock,Command:" & """" & ListBox1.Items.Item(0) & """" & "}"
+
+            '第二 ~ 倒數第二個指令
+            For i = 1 To ListBox1.Items.Count - 2
+                TextBox1.Text = TextBox1.Text & ",id:MinecartCommandBlock,Command:" & """" & ListBox1.Items.Item(i) & """" & "}"
+            Next
+
+            '最後一個指令
+            TextBox1.Text = TextBox1.Text & ",Command:" & """" & ListBox1.Items.Item(ListBox1.Items.Count - 1) & """" & "}"
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ListBox1.Items.Count = 0 Then
+            MsgBox("嗯...請先加幾個指令再用此功能吧!")
+            Exit Sub
+        End If
+        SaveFileDialog1.ShowDialog()
+        If SaveFileDialog1.FileName = "" Then Exit Sub
+        If My.Computer.FileSystem.FileExists(SaveFileDialog1.FileName) Then My.Computer.FileSystem.DeleteFile(SaveFileDialog1.FileName)
+        Dim commands As String = ""
+        For i = 0 To ListBox1.Items.Count - 1
+            If i = ListBox1.Items.Count - 1 Then
+                commands = commands & ListBox1.Items.Item(i)
+            Else
+                commands = commands & ListBox1.Items.Item(i) & vbCrLf
+            End If
+        Next
+        My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, TextBox2.Text & vbCrLf & TextBox3.Text & vbCrLf & TextBox4.Text & vbCrLf & commands, False)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+        If OpenFileDialog1.FileName = "" Then Exit Sub
+        If Not My.Computer.FileSystem.FileExists(OpenFileDialog1.FileName) Then
+            MsgBox("指定了不存在的檔案  或是使用者取消動作")
+            Exit Sub
+        End If
+        ListBox1.Items.Clear()
+        Dim allline As String() = System.IO.File.ReadAllLines(OpenFileDialog1.FileName)
+        TextBox2.Text = allline(0)
+        TextBox3.Text = allline(1)
+        TextBox4.Text = allline(2)
+        For i = 3 To allline.Length - 1
+            ListBox1.Items.Add(allline(i))
+        Next
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged, Me.Load
+        If TextBox1.Text = "" Then
+            Button9.Enabled = False
+        Else
+            Button9.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        My.Computer.Clipboard.SetText(TextBox1.Text)
+    End Sub
+End Class
